5dd3432ebdf6b0635c8b151c9ba66ca5d40ff4aa
Galihbhr <137261541+Galihbhr@users.noreply.github.com>
2024-11-19 22:42:52 +0800

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -8,16 +8,12 @@ sns.set(style='dark')
 all_df = pd.read_csv("https://raw.githubusercontent.com/Galihbhr/dashboard-ecommerce-main/refs/heads/main/dashboard/all_df.csv")
 st.header('Ecommerce Dashboard :sparkles:')
 
-
+# Top 10 Product
 st.subheader('Top 10 Product')
-# col1, col2 = st.columns(2)
-# Mengelompokkan data berdasarkan kategori produk dan menghitung jumlah item terjual
+
 product_sold_df = all_df.groupby('product_category_name_english')['order_item_id'].sum().sort_values(ascending=False)
-# Mengambil 10 produk teratas berdasarkan jumlah item terjual
 top_10_products = product_sold_df.head(10)
 
-# with col1:
-    # Menampilkan 10 produk teratas di Streamlit sebagai tabel
 st.write("Tabel Top 10 Products Sold:")
 st.table(top_10_products.reset_index().rename(columns={
     'product_category_name_english': 'Product Category',
@@ -25,52 +21,39 @@ st.table(top_10_products.reset_index().rename(columns={
 }))
 
 st.write('Grafik Top 10 Product')
-#  grafik:
-fig, ax = plt.subplots(figsize=(100, 50))
+fig, ax = plt.subplots(figsize=(15, 8))
 
 sns.barplot(
     x=top_10_products.values, 
-    y=top_10_products.index, 
-    palette="viridis"
+    y=top_10_products.index
 )
-    
-    # Customize the plot
-ax.set_title("Top 10 Product", loc="center", fontsize=150)
-ax.set_ylabel('Nama Product', loc= 'center', fontsize=150)
-ax.set_xlabel('jumlah product', loc= 'center', fontsize=150)
-ax.tick_params(axis='y', labelsize=100)
-ax.tick_params(axis='x', labelsize=100)
 
-    # Show the Streamlit app
+ax.set_title("Top 10 Product", ha="center", fontsize=20)
+ax.set_ylabel('Nama Product', fontsize=15)
+ax.set_xlabel('Jumlah Product', fontsize=15)
+ax.tick_params(axis='y', labelsize=10)
+ax.tick_params(axis='x', labelsize=10)
+
 st.pyplot(fig)
 
-st.subheader('Penjualan pertahun')
+# Penjualan pertahun
+st.subheader('Penjualan per Tahun')
 
-# Konversi kolom 'order_purchase_timestamp' menjadi datetime
 all_df['order_purchase_timestamp'] = pd.to_datetime(all_df['order_purchase_timestamp'])
-
-# Tambahkan kolom year_month
 all_df['year_month'] = all_df['order_purchase_timestamp'].dt.to_period('M')
 
-# Cetak hasil untuk memastikan
-print(all_df[['order_purchase_timestamp', 'year_month']].head())
-
-# Mengelompokkan data berdasarkan tahun dan bulan, menghitung jumlah entri per kombinasi
 monthly_counts = all_df.groupby('year_month')['order_item_id'].count()
 monthly_counts_df = monthly_counts.reset_index()
 monthly_counts_df.columns = ['year_month', 'order_item_id']
-
-# Pastikan kolom 'year_month' diubah ke format datetime
 monthly_counts_df['year_month'] = pd.to_datetime(monthly_counts_df['year_month'].astype(str))
 
-fig, ax = plt.subplots(figsize=(100, 50))
+fig, ax = plt.subplots(figsize=(15, 8))
 sns.barplot(x='year_month', y='order_item_id', data=monthly_counts_df)
-    # Customize the plot
-ax.set_title("Penjualan", loc="center", fontsize=150)
-ax.set_ylabel('Jumlah', loc= 'center', fontsize=150)
-ax.set_xlabel('Tahun-Bulan', loc= 'center', fontsize=150)
-ax.tick_params(axis='y', labelsize=100)
-ax.tick_params(axis='x', labelsize=50, rotation=45)
 
-    # Show the Streamlit app
+ax.set_title("Penjualan", ha="center", fontsize=20)
+ax.set_ylabel('Jumlah', fontsize=15)
+ax.set_xlabel('Tahun-Bulan', fontsize=15)
+ax.tick_params(axis='y', labelsize=10)
+ax.tick_params(axis='x', labelsize=10, rotation=30)
+
 st.pyplot(fig)
